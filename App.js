c677e6dd19d59626366df5e30111ba9102377c2c
Katie Barriere <kbarriere@gmail.com>
2020-12-12 08:09:10 -0500

    Add a component to add items

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Alert } from 'react-native';
 //import { uuid } from 'uuidv4';
 
 import Header from './components/Header';
 import ListItem from './components/ListItem';
+import AddItem from './components/AddItem';
 
 const App = () => {
     const [items, setItems] = useState([
@@ -25,15 +26,32 @@ const App = () => {
         },
     ]);
 
+    const deleteItem = (id) => {
+        setItems(prevItems => {
+            return prevItems.filter(item => item.id != id);
+        });
+    }
+
+    const addItem = (text) => {
+        if (!text) {
+            Alert.alert('Error', 'Please enter an item', { text: 'OK' });
+        } else {
+            setItems(prevItems => {
+                return [{ id: Math.floor(Math.random() * 100) + 1, text }, ...prevItems];
+            });
+        }
+    }
+
     return (
         <View style={styles.container}>
             <Header title="Shopping List" />
+            <AddItem addItem={addItem} />
             <FlatList
                 data={items}
                 renderItem={({ item }) => (
                     <ListItem
                         item={item}
-
+                        deleteItem={deleteItem}
                     />
                 )} />
         </View>
